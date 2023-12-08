55bb94bd4a5c7342838b8f1f46fa00b8049b1da7
ShannenLim <83536103+ShannenLim@users.noreply.github.com>
2023-12-08 16:32:24 +0800

    .

--- Assets/Phishing Scam/Scripts/ReportButton.cs
+++ Assets/Phishing Scam/Scripts/ReportButton.cs
@@ -10,20 +10,20 @@ public class ReportButton : MonoBehaviour
     //    SceneManager.LoadScene(sceneID);
     //}
 
-    public GameObject gameObject;
+    public GameObject gameObject;  //panel
     bool active;
 
-    public void OpenAndClose()
+    public void OpenAndClose()     //open and close report screen, press report button to activiate report screen
     {
         if (active == false)
         {
             gameObject.transform.gameObject.SetActive(true);
             active = true;
         }
-        else
-        {
-            gameObject.transform.gameObject.SetActive(false);
-            active = false;
-        }
+        //else
+        //{
+        //    gameObject.transform.gameObject.SetActive(false);
+        //    active = false;
+        //}
     }
 }
