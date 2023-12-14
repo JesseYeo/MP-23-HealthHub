97939dd807789f9ac052e27ef0a1c0f5e0ffd0aa
ShannenLim <83536103+ShannenLim@users.noreply.github.com>
2023-12-14 12:58:36 +0800

    Comic Story

--- Assets/Phishing Scam/Scripts/StartingStory.cs
+++ Assets/Phishing Scam/Scripts/StartingStory.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Level1MenuStartButton : MonoBehaviour
+public class StartingStory : MonoBehaviour
 {
-    public void MoveToStartingStory(int sceneID)
+    public void MoveToScamNotification(int sceneID)
     {
         SceneManager.LoadScene(sceneID);
     }
