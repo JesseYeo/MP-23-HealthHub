7189c0cb0a0031c936b4a45026b98beaeafa8250
ShannenLim <83536103+ShannenLim@users.noreply.github.com>
2024-01-11 14:31:29 +0800

    .

--- Assets/Menus/Scripts/SceneRandomiser.cs
+++ Assets/Menus/Scripts/SceneRandomiser.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneRandomiser : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    string[] scams = new string[3] { "Phishing Scam", "Fake Friend Scam", "Malware Scam" };
+    int random = Random.Range(0, 3);
+    //Application.LoadLevel(scams[random]);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+     //string[] zones = new string[3] { "zoneA", "zoneB", "zoneC" };
+    //int random = Random.Range(0, 3);
+    //Application.LoadLevel(zones[random]);
 }
