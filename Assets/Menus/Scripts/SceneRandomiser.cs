2fcf22941edce422dbb43fb6bec8ed318ea7beb4
Jesse Yeo <mrjesseyeo@gmail.com>
2024-01-15 16:46:16 +0800

    Coroutine for level 4

--- Assets/Menus/Scripts/SceneRandomiser.cs
+++ Assets/Menus/Scripts/SceneRandomiser.cs
@@ -7,15 +7,22 @@ public class SceneRandomiser : MonoBehaviour
 {
 
     public int[] scenes;
-
-
+    
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        int rand = Random.Range(0,scenes.Length);
 
-        SceneManager.LoadScene(scenes[rand]);
+
+        //int rand = Random.Range(0,scenes.Length);
+
+        //SceneManager.LoadScene(scenes[rand]);
+
+        //int[] current = scenes;
+
+        SceneManager.LoadScene(60);
+
     }
 
     // Update is called once per frame
@@ -23,4 +30,7 @@ public class SceneRandomiser : MonoBehaviour
     {
         
     }
+
+
+
 }
